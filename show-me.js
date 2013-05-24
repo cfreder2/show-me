48dc6c0eef9896839bfb30935acb82a935982c89
Paul Berden <paulberden@gmail.com>
2013-05-24 19:02:39 +0200

    Managed to access the nested array untill the deepest level. Woohoo, breakthrough

--- show-me.js
+++ show-me.js
@@ -5,7 +5,7 @@ var width = 500,
     height = 500, 
     margin = 50;
 
-var cwidth=100,cheight=100,cmargin=5,maxr=5;
+var cwidth=300,cheight=100,cmargin=5,maxr=5;
 
 var svg=d3.select("body").append("svg");
 var x=d3.scale.linear().domain([0,5]).range([cmargin,cwidth-margin]);
@@ -31,9 +31,41 @@ console.log(data);
   var g=svg.selectAll("g").data(data).enter()
     .append("g")
     .attr("transform",function(d,i) {return "translate(0,"+(cheight*i)+")";});
+  g
+    .append("rect")
+    .attr("x",cmargin)
+    .attr("y",cmargin)
+    .attr("width",cwidth-2*cmargin)
+    .attr("height",cheight-2*cmargin)
+      .append("title")
+      .text(function(d) {return d.key;})
+  // we also write its name below.
+  g
+    .append("text")
+    .attr("y",cheight+10)
+    .attr("x",cmargin)
+    .text(function(d) {return d.key;})
 
-   
+  // create subgroups for the categories
+  g
+  .selectAll("g")
+  // we are getting the values of the categories like this:
+  .data(function(d) {return d.values}) 
+  .enter()
+      .append("g")
+      .attr("class",function(d) {return d.key;})
+
+//TODO -> figure out why the &%@#$ it does not work when the variable g is added... Perhaps I'm too tired, heck this feels like talking to my self. Is'nt that the first sign of going mental?
+    .selectAll("rect")
+    .data(function(d) {return d.values})
+    .enter()
+    .append("rect")
+    .attr("y", 0)
+    .attr("x", function (d,i) {return i*25})
+    .attr("width", 20)
+    .attr("height", function(d) {return d.values.Value / 3})
+    .attr("class", function(d) {return d.key;});
+
 })
-
 }
 
